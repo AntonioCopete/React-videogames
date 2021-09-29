e3fd6cd7db1b0c603fe435cc47a51e8ccbfc6997
AntonioCopete <antcopjim@gmail.com>
2021-09-29 17:26:00 +0200

    fixed gameDetail state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,50 @@ import GameDetails from "./pages/GameDetails";
 import React from "react";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gameDetailed: {},
+    };
+
+    this.getGameDetails = this.getGameDetails.bind(this);
+  }
+
+  getGameDetails(id) {
+    return fetch(
+      `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+          "x-rapidapi-key":
+            "23e233b049msh4485b68fa7318bdp11fd05jsn2ac4d49d92a1",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ gameDetailed: data });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
   render() {
+    console.log(this.state.gameDetailed);
+    const {
+      title,
+      thumbnail,
+      description,
+      genre,
+      platform,
+      publisher,
+      developer,
+      release_date,
+    } = this.state.gameDetailed;
     return (
       <BrowserRouter>
         <header>
@@ -18,7 +61,20 @@ export default class App extends React.Component {
           <Route
             exact
             path="/games/:gameId"
-            render={(routeProps) => <GameDetails {...routeProps} />}
+            render={(routeProps) => (
+              <GameDetails
+                {...routeProps}
+                title={title}
+                thumbnail={thumbnail}
+                description={description}
+                genre={genre}
+                platform={platform}
+                publisher={publisher}
+                developer={developer}
+                releaseDate={release_date}
+                handleDetails={this.getGameDetails}
+              />
+            )}
           ></Route>
 
           <Route path="/">
--- src/pages/GameDetails/GameDetails.jsx
+++ src/pages/GameDetails/GameDetails.jsx
@@ -7,35 +7,12 @@ export default class MyComponent extends React.Component {
     this.state = {
       game: {},
     };
-
-    this.getGameDetails = this.getGameDetails.bind(this);
   }
 
   componentDidMount() {
-    this.getGameDetails(this.props.match.params.gameId);
+    this.props.handleDetails(this.props.match.params.gameId);
   }
-  getGameDetails(id) {
-    return fetch(
-      `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`,
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-          "x-rapidapi-key":
-            "23e233b049msh4485b68fa7318bdp11fd05jsn2ac4d49d92a1",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({ game: data });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
+
   render() {
     const {
       title,
@@ -46,7 +23,7 @@ export default class MyComponent extends React.Component {
       publisher,
       developer,
       release_date,
-    } = this.state.game;
+    } = this.props;
     return (
       <>
         <div className="game-details">
