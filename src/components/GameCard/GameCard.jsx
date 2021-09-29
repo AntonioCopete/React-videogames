f4c00e08b59188853accbf5e7873d8d59015e11d
AntonioCopete <antcopjim@gmail.com>
2021-09-29 13:39:19 +0200

    added gameCard url

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,22 +1,35 @@
-import React, { Component } from 'react'
-import "./GameCard.scss"
+import React, { Component } from "react";
+import "./GameCard.scss";
+import { Link } from "react-router-dom";
 
 export default class GameCard extends Component {
-    render() {
-        const {title, thumbnail, description, genre, platform, publisher, developer, releaseDate} = this.props.game
-        console.log(this.props.game);
-        return (
-            <li className="game-card">
-                <h2>{title}</h2>
-                <img src={thumbnail} alt={title} />
-                {/* <p><span><b>Description:</b></span> {description}</p> */}
-                {/* <p><span><b>Genre:</b></span> {genre}</p> */}
-                {/* <p><span><b>Platform:</b></span> PC (Windows)</p> */}
-                {/* <p><span><b>Publisher:</b></span> Phoenix Labs</p> */}
-                {/* <p><span><b>Developer:</b></span> Phoenix Labs, Iron Galaxy</p> */}
-                {/* <p><span><b>Release date:</b></span> 2019-05-21</p> */}
-                {/* <p><span><b>Links:</b></span> <a href="https://www.freetogame.com/dauntless">Freetogame</a><a href="https://www.freetogame.com/open/dauntless">Official Website</a></p> */}
-            </li>
-        )
-    }
+  render() {
+    const {
+      id,
+      title,
+      thumbnail,
+      description,
+      genre,
+      platform,
+      publisher,
+      developer,
+      releaseDate,
+    } = this.props.game;
+    console.log(this.props.game);
+    return (
+      <li className="game-card" key={id}>
+        <Link to={"/games/" + id}>
+          <h2>{title}</h2>
+          <img src={thumbnail} alt={title} />
+          {/* <p><span><b>Description:</b></span> {description}</p> */}
+          {/* <p><span><b>Genre:</b></span> {genre}</p> */}
+          {/* <p><span><b>Platform:</b></span> PC (Windows)</p> */}
+          {/* <p><span><b>Publisher:</b></span> Phoenix Labs</p> */}
+          {/* <p><span><b>Developer:</b></span> Phoenix Labs, Iron Galaxy</p> */}
+          {/* <p><span><b>Release date:</b></span> 2019-05-21</p> */}
+          {/* <p><span><b>Links:</b></span> <a href="https://www.freetogame.com/dauntless">Freetogame</a><a href="https://www.freetogame.com/open/dauntless">Official Website</a></p> */}
+        </Link>
+      </li>
+    );
+  }
 }
